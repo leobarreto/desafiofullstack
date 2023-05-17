6ae2eb8028f4be26204572b69ec2c6dc5e119eed
Leonardo Barreto <oterrab@gmail.com>
2023-05-17 09:46:03 -0300

    Desafio Full Satck - Senior

---
+++ desafiosenior/src/main/java/com/leobarreto/desafiosenior/DesafioseniorApplication.java
@@ -0,0 +1,13 @@
+package com.leobarreto.desafiosenior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioseniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioseniorApplication.class, args);
+	}
+
+}
---
+++ desafiosenior/src/test/java/com/leobarreto/desafiosenior/DesafioseniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leobarreto.desafiosenior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioseniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
